6e7e916956f136544f2a20983f601f8419aee4dd
Ievgenii Kaieta <kaieta.ievgenii@gmail.com>
2023-11-17 20:23:54 +0200

    fixing variable port and host for heroku

--- apps/client/src/with-graphql/client.ts
+++ apps/client/src/with-graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri:  `${import.meta.env.VITE_HOST}:${import.meta.env.VITE_PORT}/api/graphql`,
+    uri:  `${import.meta.env.VITE_HOST}:${import.meta.env.VITE_PORT || 3000}/api/graphql`,
     cache: new InMemoryCache()
 });
 
